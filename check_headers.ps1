c02977adcb71f8389fb659bb76b757a71eb4eec4
Khang VieX <khang@viex.vn>
2026-07-15 20:50:21 +0700

    Remove development and test scripts

--- check_headers.ps1
+++
@@ -1,2 +0,0 @@
-$response = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/viexdigital001/Valorant-Optimizer/main/core/OptimizeEngine.ps1' -Method Head
-$response.Headers | Out-String
--- find_non_ascii.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -Path '.' -Filter '*.ps1' -Recurse | ForEach-Object {
-    $content = Get-Content $_.FullName -Raw -Encoding UTF8
-    if ($content -match '[^\x00-\x7F]') {
-        Write-Host $_.FullName
-    }
-}
